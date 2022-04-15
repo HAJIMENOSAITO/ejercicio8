4eb7c1290ff65e1f6b4cc7d0cf55fc0df727b124
HAJIMENOSAITO <103212000+HAJIMENOSAITO@users.noreply.github.com>
2022-04-14 23:51:20 -0400

    Add files via upload

---
+++ ejercicio8/src/ejercicio8/Ejercicio8.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio8;
+
+/**
+ *
+ * @author emil
+ */
+public class Ejercicio8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           int numero = 1;
+        
+        while(numero <= 100) {
+         numero++;
+        if(numero %2 == 0 ){System.out.println(numero);}
+        else if(numero %3 == 0 ){System.out.println(numero);}
+        
+        }
+    }   
+}
